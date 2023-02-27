c7a6566348d112cb5dde861fc2d0aa8f58c9f336
PMS <ms147850@gmail.com>
2023-02-28 00:09:55 +0900

    feat: add axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestMsg msg="Test!!!!!!!!!"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestMsg from "@/components/TestMsg";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestMsg
   }
 }
 </script>
